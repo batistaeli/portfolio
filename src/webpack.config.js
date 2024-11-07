8d52b720173712c4b10405d8dea7093cdbba85ce
Pedro Leda <pedro.henrique.leda@gmail.com>
2024-11-07 01:51:02 -0300

    colocando no ar v1

---
+++ src/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+              // options...
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin(), new TerserJSPlugin({})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].bundle.css",
+    }),
+  ],
+};
